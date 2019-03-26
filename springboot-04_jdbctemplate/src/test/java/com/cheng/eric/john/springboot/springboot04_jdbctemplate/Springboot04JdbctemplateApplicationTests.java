e3be91d1e20cb4409ea88a777bd91b7833446263
JohnEricCheng <dong19910925@126.com>
2019-03-26 10:50:08 +0800

    创建JdbcTemplate项目。

---
+++ springboot-04_jdbctemplate/src/test/java/com/cheng/eric/john/springboot/springboot04_jdbctemplate/Springboot04JdbctemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cheng.eric.john.springboot.springboot04_jdbctemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot04JdbctemplateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
